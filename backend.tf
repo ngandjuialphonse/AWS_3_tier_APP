c4e16410d429aa4aeda81dfc3f80cb9b04f01c64
ngandjuialphonse <114841403+ngandjuialphonse@users.noreply.github.com>
2023-09-14 10:31:55 -0500

    Update backend.tf
    Change Tag name

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "utrains-bucket-backend-three-tier-app"
+    bucket         = "alphodine-bucket-backend-three-tier-app"
     encrypt        = true
     key            = "terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "utrains-dynamodb-backend-three-tier-app"
+    dynamodb_table = "alphodine-dynamodb-backend-three-tier-app"
   }
 }
